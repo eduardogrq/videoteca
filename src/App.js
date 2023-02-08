e18eaa072fdd41ebbf5109c9280143204d7a7ec0
Eduardogrq <eduardo.grquinonez@gmail.com>
2023-02-08 16:27:31 -0600

    wip: adding Header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="flex justify-around">
-        <h2 className="text-2xl font-bold">Mi logo</h2>
-        <ul className="flex gap-3">
-
-          <li>Primer link</li>
-          <li>Primer link</li>
-        </ul>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+    return (
+        <header className="flex justify-around">
+            <h2 className="text-2xl font-bold">Mi logo</h2>
+            <ul className="flex gap-3">
+
+                <li>Primer link</li>
+                <li>Primer link</li>
+            </ul>
+        </header>
+    )
+}
+
+export default Header;
