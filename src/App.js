886b0f19b27f9e2b608a1538e325309d67da83ee
Eduardogrq <eduardo.grquinonez@gmail.com>
2023-02-09 12:19:47 -0600

    Adding signOut method to cognito pool users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 
+import { Hub } from "aws-amplify";
+import { useEffect } from "react";
 import AppRouter from "./routes/AppRouter";
 
 function App() {
+
+  // useEffect(() => {
+  //   Hub.listen('auth', event => {
+  //     console.log('auth event', event)
+  //   })
+  // })
+
   return (
     <div>
       <AppRouter />
--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,5 +1,5 @@
 
-import {Amplify, Auth} from 'aws-amplify';
+import { Amplify, Auth } from 'aws-amplify';
 import awsconfig from '../../aws-exports';
 
 import { useState } from 'react';
@@ -11,23 +11,35 @@ const Login = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleSubmit = async(event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
 
         let response = await Auth.signIn(email, password);
         console.log('respuestita', response);
     }
 
-    return(
-        <form className="container mx-auto" onSubmit={handleSubmit}>
-            <label htmlFor="">Email</label>
-            <input type="text" onChange={(event) => setEmail(event.target.value)}/>
-            <hr />
-            <label htmlFor="">Password</label>
-            <input type="password" onChange={(event) => setPassword(event.target.value)}/>
-            <hr />
-            <button className="btn btn-primary" type="submit">Enviar</button>
-        </form>
+    const signOut = async() => {
+        try {
+            await Auth.signOut();
+          } catch (error) {
+            console.error(error);
+          }
+    }
+
+    return (
+        <div className='container mx-auto'>
+            <form className="container mx-auto" onSubmit={handleSubmit}>
+                <label htmlFor="">Email</label>
+                <input type="text" onChange={(event) => setEmail(event.target.value)} />
+                <hr />
+                <label htmlFor="">Password</label>
+                <input type="password" onChange={(event) => setPassword(event.target.value)} />
+                <hr />
+                <button className="btn btn-primary" type="submit">Enviar</button>
+            </form>
+
+            <button onClick={signOut}>Cerrar sesión</button>
+        </div>
     )
 }
 
