8acb5e1c0f4a29da0b9906205c59a9c19ae94234
Eduardogrq <eduardo.grquinonez@gmail.com>
2023-02-10 10:39:23 -0600

    wip: adding cognito auth methods and solving padding errors in login template

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -26,7 +26,7 @@ const Login = () => {
 
     return (
         <div className="flex h-screen">
-            <section className="w-1/2 h-full p-4">
+            <section className="w-1/2 h-full">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900">
                         <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
