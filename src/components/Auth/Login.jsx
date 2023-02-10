e2ff0f9990bf78a553c42a30fb7befb3243da07f
Eduardogrq <eduardo.grquinonez@gmail.com>
2023-02-09 18:57:08 -0600

    feature: adding login component and its styles.

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -2,7 +2,6 @@
 import { Amplify, Auth, Hub } from 'aws-amplify';
 import awsconfig from '../../aws-exports';
 import bgImage from './../../images/bg-login.png';
-
 import { useState } from 'react';
 
 Amplify.configure(awsconfig);
@@ -17,19 +16,19 @@ const Login = () => {
                         <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
                         Flowbite
                     </a>
-                    <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="w-full bg-white rounded-lg shadow-lg md:mt-0 sm:max-w-md xl:p-0">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Iniciar sesión
+                            <h1 className="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                                Iniciar Sesión
                             </h1>
                             <form className="space-y-4 md:space-y-6" action="#">
                                 <div>
-                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Email</label>
-                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="name@company.com" required="" />
+                                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">Correo electrónico</label>
+                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="john@email.com" required="" />
                                 </div>
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 ">Contraseña</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  " required="" />
+                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" required="" />
                                 </div>
                                 <div className="flex items-center justify-between">
                                     <div className="flex items-start">
@@ -37,21 +36,25 @@ const Login = () => {
                                             <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 " required="" />
                                         </div>
                                         <div className="ml-3 text-sm">
-                                            <label htmlFor="remember" className="text-gray-500">Recordar</label>
+                                            <label htmlFor="remember" className="text-gray-500">Recordar sesión</label>
                                         </div>
                                     </div>
-                                    <a href="#" className="text-sm font-medium bluetext text-cyan-500 hover:underline">Forgot password?</a>
+                                    <a href="#" className="text-sm font-medium bluetext text-blue-500 hover:underline">¿Olvidaste tu contraseña?</a>
                                 </div>
-                                <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
-                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                                <div>
+                                    <button className='bg-blue-500 w-full hover:bg-blue-700 text-white font-bold py-3 rounded'>Entrar</button>
+                                </div>
+                                <hr />
+                                {/* <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 pb-2.5 text-center">Sign in</button> */}
+                                <p className="text-sm text-center font-light text-gray-500">
+                                    ¿Aún no tienes cuenta? <a href="#" className="font-medium text-blue-500 hover:underline">Regístrate</a>
                                 </p>
                             </form>
                         </div>
                     </div>
                 </div>
             </section>
-            <section className="bg-cover bg-no-repeat w-1/2 h-full" style={{backgroundImage: `url(${bgImage})`}}>
+            <section className="bg-cover bg-no-repeat w-1/2 h-full" style={{ backgroundImage: `url(${bgImage})` }}>
             </section>
         </div>
     )
