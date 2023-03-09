90043a978efa106ad968232bb8a80f4dd7941357
Eduardogrq <eduardo.grquinonez@gmail.com>
2023-03-09 17:30:36 -0600

    fix(sidebar): solving html structure and sidebar
    solving html structure and sidebar

--- src/components/layouts/AdminLayout/AdminLayout.jsx
+++ src/components/layouts/AdminLayout/AdminLayout.jsx
@@ -12,29 +12,15 @@ const AdminPanelLayout = ({ children }) => {
     }
 
     return (
-        // <div className="lg:flex">
-        //     {/* Sidebar component */}
-        //     <Sidebar isHidden={isHidden} turnToHidden={turnToHidden} />
-        //     {/* Main container */}
-        //     <main className="h-screen w-screen lg:relative bg-gray-200">
-        //         {/* Navbar component */}
-        //         <Navbar turnToHidden={turnToHidden} />
-        //         {/* Content */}
-        //         <div className="container px-4 py-6">
-        //             {children}
-        //         </div>
-        //     </main>
-        // </div>
 
         <div className="flex h-screen">
 
             <Sidebar isHidden={isHidden} turnToHidden={turnToHidden}/>
             {/* Contenido principal */}
-            <div className={`flex-1 bg-red-200 ${isHidden || 'lg:ml-64'} `}>
-
+            <div className={`flex-1  ${isHidden || 'lg:ml-64'} `}>
                 <Navbar turnToHidden={turnToHidden} />
 
-                <div className="p-10 max-w-screen-2xl">
+                <div>
                     {children}
                 </div>
             </div>
--- src/pages/AdminPanel/Dashboard/Dashboard.jsx
+++ src/pages/AdminPanel/Dashboard/Dashboard.jsx
@@ -6,13 +6,26 @@ const Dashboard = () => {
   return (
 
     <AdminLayout>
-      <div className="grid grid-cols-4">
+      <div className="grid grid-cols-4 bg-red-500">
         <div className="col-span-3 bg-gray-200 h-64"></div>
         <div className="col-span-1 bg-red-200 h-64"></div>
         <div className="col-span-1 bg-yellow-200 h-64"></div>
         <div className="col-span-3 bg-purple-200 h-64"></div>
         <div className="col-span-2 bg-blue-200 h-64"></div>
         <div className="col-span-2 bg-orange-200 h-64"></div>
+        <div className="col-span-3 bg-gray-200 h-64"></div>
+        <div className="col-span-1 bg-red-200 h-64"></div>
+        <div className="col-span-1 bg-yellow-200 h-64"></div>
+        <div className="col-span-3 bg-purple-200 h-64"></div>
+        <div className="col-span-2 bg-blue-200 h-64"></div>
+        {/* <div className="col-span-2 bg-orange-200 h-64"></div>
+        <div className="col-span-3 bg-gray-200 h-64"></div>
+        <div className="col-span-1 bg-red-200 h-64"></div>
+        <div className="col-span-1 bg-yellow-200 h-64"></div>
+        <div className="col-span-3 bg-purple-200 h-64"></div>
+        <div className="col-span-2 bg-blue-200 h-64"></div>
+        <div className="col-span-2 bg-orange-200 h-64"></div> */}
+
       </div>
     </AdminLayout>
   )
