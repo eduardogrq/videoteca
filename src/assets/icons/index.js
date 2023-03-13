87ff2505a9fb2fe1d55b8d43ad47f86df0f8da70
Eduardogrq <eduardo.grquinonez@gmail.com>
2023-03-13 10:54:03 -0600

    refactor(peopleicon): split peopleIcon from dashboard
    split peopleIcon from dashboard

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,9 +2,11 @@
 import ViewIcon from "./ViewIcon/ViewIcon";
 import HideViewIcon from "./HideViewIcon/HideViewIcon";
 import CheckIcon from "./CheckIcon/CheckIcon";
+import PeopleIcon from "./PeopleIcon/PeopleIcon";
 
 export {
     ViewIcon,
     HideViewIcon,
     CheckIcon,
+    PeopleIcon
 }
