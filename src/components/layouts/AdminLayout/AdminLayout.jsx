5120a8466f29d946f6205eb0361b3c450fd86052
Eduardogrq <eduardo.grquinonez@gmail.com>
2023-03-14 13:43:32 -0600

    feat(sidebar): new sidebar
    new sidebar

--- src/components/layouts/AdminLayout/AdminLayout.jsx
+++ src/components/layouts/AdminLayout/AdminLayout.jsx
@@ -1,13 +1,15 @@
 
 import { useState, useContext } from "react";
+import { DataContext } from "./../../../context/DataContext";
+import { XMarkIcon } from "@heroicons/react/24/outline";
 import Navbar from './Navbar/Navbar';
 import Sidebar from "./Sidebar/Sidebar";
-import {DataContext} from "./../../../context/DataContext";
+
 
 const AdminPanelLayout = ({ children }) => {
 
-    const {userData, setUserData} = useContext(DataContext)
-    
+    const { userData, setUserData } = useContext(DataContext)
+
     const [isHidden, setIsHidden] = useState(true)
 
     const turnToHidden = () => {
@@ -15,19 +17,53 @@ const AdminPanelLayout = ({ children }) => {
     }
 
     return (
+        <div className="flex min-h-screen">
+            {/* sidebar */}
+            <div className={`bg-red-500 fixed top-0 ${isHidden ? '-left-full' : 'left-0'} lg:static lg:w-80 w-[75%] md:w-[40%] overflow-y-scroll z-50 transition-all`}>
+                {/* avatar section */}
 
-        <div className="flex h-screen">
-            <Sidebar isHidden={isHidden} turnToHidden={turnToHidden} userAttributes={userData} />
-            {/* Contenido principal */}
-            <div className={`flex-1  ${isHidden || 'lg:ml-64'} `}>
-                
-                <Navbar turnToHidden={turnToHidden} isHidden={isHidden} userAttributes={userData} />
+                <div className="flex p-5 flex-col justify-between min-h-screen">
+                    <div>
+                        <div className="flex items-center justify-between">
+                            <img src="https://picsum.photos/50/50" alt="" className="rounded-full"/>
+                            <span className="text-sm">Eduardo Quiñonez</span>
+                            <XMarkIcon className="w-6 h-6" onClick={turnToHidden}/>
+                        </div>
+                        <ul>
+                            <li>Primero</li>
+                            <li>Primero</li>
+                            <li>Primero</li>
+                            <li>Primero</li>
+                            <li>Primero</li>
+                        </ul>
+                    </div>
 
-                <div className="bg-red-500">
-                    {children}
+                    <ul>
+                        <li>segundo</li>
+                        <li>segundo</li>
+                        <li>segundo</li>
+                        <li>segundo</li>
+                    </ul>
                 </div>
             </div>
+            <div className="w-full col-span-12 bg-red-200">
+                <button onClick={turnToHidden}>yei</button>
+            </div>
         </div>
+
+        // <div className="flex h-screen">
+        //     <Sidebar isHidden={isHidden} turnToHidden={turnToHidden} userAttributes={userData} />
+        //     {/* Contenido principal */}
+        //     <div className={`flex-1  ${isHidden || 'lg:ml-64'} `}>
+
+        //         <Navbar turnToHidden={turnToHidden} isHidden={isHidden} userAttributes={userData} />
+
+        //         <div className="bg-red-500">
+        //             {children}
+        //         </div>
+        //     </div>
+        // </div>
+
 
     );
 }
--- src/components/layouts/AdminLayout/Sidebar/Sidebar.jsx
+++ src/components/layouts/AdminLayout/Sidebar/Sidebar.jsx
@@ -15,94 +15,99 @@ const Sidebar = ({ isHidden, turnToHidden }) => {
 
     return (
         // {/* Sidebar */}
-        <div className="flex min-h-screen">
-            <div 
-                className={`min-h-screen w-1/5 md:w-3/5 fixed right-0 bg-black opacity-40 ${!isHidden ? 'block lg:hidden' : 'hidden'}`}
-                onClick={turnToHidden}
-            >
-            </div>
-            <aside className={`w-4/5 md:w-2/5 lg:w-64 text-white fixed min-h-screen inset-y-0 left-0 bg-gray-800 ${!isHidden || 'hidden'}`}>
-                <div className="h-full bg-gray-800 pt-0 px-5">
-                    <div className="flex justify-between items-center w-full h-20 overflow-hidden">
-                        <div className="flex items-center lg:justify-center lg:text-center lg:w-full gap-5 cursor-pointer">
-                            <img src="https://picsum.photos/seed/picsum/40/40"
-                                className="text-lg rounded-full lg:hidden"
-                                alt=""
-                            />
-                            <img src="https://picsum.photos/seed/picsum/120/50"
-                                className="text-lg rounded-full hidden lg:block"
-                                alt=""
-                            />
-                            <span className="lg:text-xs lg:hidden whitespace-pre-wrap">Eduardo Quiñonez</span>
-                        </div>
-                        <XMarkIcon className="w-6 h-6 lg:hidden cursor-pointer" onClick={turnToHidden} />
-                    </div>
-                    <div className="h-screen flex flex-col justify-between">
-                        {/* First block */}
-                        <div className="w-full h-1/2 pt-8">
-                            <ul className='space-y-10 lg:space-y-8'>
-                                <li className='flex items-center'>
-                                    <span className='cursor-pointer flex items-center'>
-                                        <Squares2X2Icon className="w-6 h-6 mr-4" />
-                                        Inicio
-                                    </span>
-                                </li>
-                                <li className="flex items-center">
-                                    <span className='cursor-pointer flex items-center'>
-                                        <PlayIcon className="w-6 h-6 mr-4" />
-                                        Colección
-                                    </span>
-                                </li>
-                                <li className="flex items-center">
-                                    <span className='cursor-pointer flex items-center'>
-                                        <MicrophoneIcon className="w-6 h-6 mr-4" />
-                                        Transcripción
-                                    </span>
-                                </li>
-                                <li className="flex items-center">
-                                    <span className='cursor-pointer flex items-center'>
-                                        <UserCircleIcon className="w-6 h-6 mr-4" />
-                                        Reconocimiento
-                                    </span>
-                                </li>
-                                <li className="flex items-center">
-                                    <span className='cursor-pointer flex items-center'>
-                                        <ArrowUpOnSquareIcon className="w-6 h-6 mr-4" />
-                                        Subir archivo
-                                    </span>
-                                </li>
-                            </ul>
-                        </div>
-                        {/* Second block */}
-                        <div className="w-full h-1/2 mb-56 lg:mb-28 flex items-end">
-                            <hr className='mb-10' />
-                            <div>
-                                <ul className='space-y-10 lg:space-y-8'>
-                                    <li className="flex items-center">
-                                        <span className='cursor-pointer flex items-center'>
-                                            <ChatBubbleBottomCenterTextIcon className="w-6 h-6 mr-4" />
-                                            Notificaciones
-                                        </span>
-                                    </li>
-                                    <li className="flex items-center">
-                                        <span className='cursor-pointer flex items-center'>
-                                            <Cog6ToothIcon className="w-6 h-6 mr-4" />
-                                            Ajustes
-                                        </span>
-                                    </li>
-                                    <li className="flex items-center">
-                                        <span className='cursor-pointer flex items-center'>
-                                            <ArrowRightOnRectangleIcon className="w-6 h-6 mr-4" />
-                                            Salir
-                                        </span>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </aside>
+        // <div className="flex min-h-screen -ml-[100px]">
+        //     <div 
+        //         // className={`min-h-screen w-1/5 md:w-3/5 fixed right-0 bg-black opacity-40 ${!isHidden ? 'block lg:hidden' : 'hidden'}`}
+        //         className={`min-h-screen w-1/5 md:w-3/5 fixed right-0 bg-black opacity-40 ${!isHidden ? 'static lg:hidden' : 'hidden'}`}
+        //         onClick={turnToHidden}
+        //     >
+        //     </div>
+        //     {/* <aside className={`w-4/5 md:w-2/5 lg:w-64 text-white fixed min-h-screen inset-y-0 left-0 bg-gray-800 ${!isHidden || 'hidden'}`}> */}
+        //     <aside className={`w-4/5 md:w-2/5 lg:w-64 text-white fixed min-h-screen inset-y-0 left-0 bg-gray-800 `}>
+        //         <div className="h-full bg-gray-800 pt-0 px-5">
+        //             <div className="flex justify-between items-center w-full h-20 overflow-hidden">
+        //                 <div className="flex items-center lg:justify-center lg:text-center lg:w-full gap-5 cursor-pointer">
+        //                     <img src="https://picsum.photos/seed/picsum/40/40"
+        //                         className="text-lg rounded-full lg:hidden"
+        //                         alt=""
+        //                     />
+        //                     <img src="https://picsum.photos/seed/picsum/120/50"
+        //                         className="text-lg rounded-full hidden lg:block"
+        //                         alt=""
+        //                     />
+        //                     <span className="lg:text-xs lg:hidden whitespace-pre-wrap">Eduardo Quiñonez</span>
+        //                 </div>
+        //                 <XMarkIcon className="w-6 h-6 lg:hidden cursor-pointer" onClick={turnToHidden} />
+        //             </div>
+        //             <div className="h-screen flex flex-col justify-between">
+        //                 {/* First block */}
+        //                 <div className="w-full h-1/2 pt-8">
+        //                     <ul className='space-y-10 lg:space-y-8'>
+        //                         <li className='flex items-center'>
+        //                             <span className='cursor-pointer flex items-center'>
+        //                                 <Squares2X2Icon className="w-6 h-6 mr-4" />
+        //                                 Inicio
+        //                             </span>
+        //                         </li>
+        //                         <li className="flex items-center">
+        //                             <span className='cursor-pointer flex items-center'>
+        //                                 <PlayIcon className="w-6 h-6 mr-4" />
+        //                                 Colección
+        //                             </span>
+        //                         </li>
+        //                         <li className="flex items-center">
+        //                             <span className='cursor-pointer flex items-center'>
+        //                                 <MicrophoneIcon className="w-6 h-6 mr-4" />
+        //                                 Transcripción
+        //                             </span>
+        //                         </li>
+        //                         <li className="flex items-center">
+        //                             <span className='cursor-pointer flex items-center'>
+        //                                 <UserCircleIcon className="w-6 h-6 mr-4" />
+        //                                 Reconocimiento
+        //                             </span>
+        //                         </li>
+        //                         <li className="flex items-center">
+        //                             <span className='cursor-pointer flex items-center'>
+        //                                 <ArrowUpOnSquareIcon className="w-6 h-6 mr-4" />
+        //                                 Subir archivo
+        //                             </span>
+        //                         </li>
+        //                     </ul>
+        //                 </div>
+        //                 {/* Second block */}
+        //                 <div className="w-full h-1/2 mb-56 lg:mb-28 flex items-end">
+        //                     <hr className='mb-10' />
+        //                     <div>
+        //                         <ul className='space-y-10 lg:space-y-8'>
+        //                             <li className="flex items-center">
+        //                                 <span className='cursor-pointer flex items-center'>
+        //                                     <ChatBubbleBottomCenterTextIcon className="w-6 h-6 mr-4" />
+        //                                     Notificaciones
+        //                                 </span>
+        //                             </li>
+        //                             <li className="flex items-center">
+        //                                 <span className='cursor-pointer flex items-center'>
+        //                                     <Cog6ToothIcon className="w-6 h-6 mr-4" />
+        //                                     Ajustes
+        //                                 </span>
+        //                             </li>
+        //                             <li className="flex items-center">
+        //                                 <span className='cursor-pointer flex items-center'>
+        //                                     <ArrowRightOnRectangleIcon className="w-6 h-6 mr-4" />
+        //                                     Salir
+        //                                 </span>
+        //                             </li>
+        //                         </ul>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </aside>
             
+        // </div>
+        <div className='full'>
+
         </div>
 
     );
